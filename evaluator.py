602a3bed5ea57170e4eb62e63def38f80d18527a
Kevin Mathew T <mathewtkevin@gmail.com>
2024-12-02 22:28:57 -0500

    debug statement

--- evaluator.py
+++ evaluator.py
@@ -114,6 +114,7 @@ class ProbingEvaluator:
                 # TODO: Forward pass through your model
                 init_states = batch.states[:, 0:1]  # BS, 1, C, H, W
                 pred_encs = model(states=init_states, actions=batch.actions, teacher_forcing=False)
+                print(f"pred_encs shape: {pred_encs.shape}")
                 pred_encs = pred_encs.transpose(0, 1)  # # BS, T, D --> T, BS, D
 
                 # Make sure pred_encs has shape (T, BS, D) at this point
