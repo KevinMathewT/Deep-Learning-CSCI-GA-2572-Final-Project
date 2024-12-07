62bbece3c5b5e3c6865a8899b4f33125f41045d6
Kevin Mathew T <mathewtkevin@gmail.com>
2024-12-06 19:44:17 -0500

    fixed 2d config file

--- train.py
+++ train.py
@@ -97,6 +97,7 @@ def train_jepa(config):
 # python -m train --config config/areg_adv_jepa_config.yaml
 # python -m train --config config/areg_adv_jepa_config_higher_delta.yaml
 # python -m train --config config/areg_adv_jepa_config_lower_delta.yaml
+# python -m train --config config/areg_jepa_2d_config.yaml
 def main():
     parser = argparse.ArgumentParser(description='Train different models.')
     parser.add_argument('--config', type=str, help='Path to configuration file')
