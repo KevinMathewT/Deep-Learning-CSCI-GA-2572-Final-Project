e63ac408d27290b3d87d41a3f42d442b1851e3a3
Kevin Mathew T <mathewtkevin@gmail.com>
2024-12-11 21:03:09 -0500

    fixed all now?

--- models.py
+++ models.py
@@ -2141,21 +2141,20 @@ class ActionRegularizationJEPA2DFlexibleEncoder(BaseModel):
         ), actions
         )  # Uses ActionRegularizer2D internally
 
-        print(f"states_embed: {states_embed.shape} | states_embed.flatten: {states_embed.flatten(start_dim=1, end_dim=-1).shape}")
         # Compute VICReg Loss
         vic_reg_loss, invariance_loss, variance_loss, covariance_loss = vicreg_loss(
-                pred_states.flatten(start_dim=1, end_dim=-1), # (B, T * C * H * W)
-                states_embed.flatten(start_dim=1, end_dim=-1), # (B, T * C * H * W)
+                pred_states.flatten(start_dim=2, end_dim=-1), # (B, T * C * H * W)
+                states_embed.flatten(start_dim=2, end_dim=-1), # (B, T * C * H * W)
             )
 
         # Compute VICReg Loss
         vic_reg_loss_s, invariance_loss_s, variance_loss_s, covariance_loss_s = vicreg_loss(
             pred_states.flatten(start_dim=0, end_dim=1)
             .permute(0, 2, 3, 1)
-            .flatten(end_dim=-2), # (B * T, H * W, C)
+            .flatten(end_dim=-2), # (B * T * H * W, C)
             states_embed.flatten(start_dim=0, end_dim=1)
             .permute(0, 2, 3, 1)
-            .flatten(end_dim=-2), # (B * T, H * W, C)
+            .flatten(end_dim=-2), # (B * T * H * W, C)
         )
 
         # Combine losses
