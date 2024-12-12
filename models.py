14bb8be96f9a1a7519763985d46a5a484cb79fdb
Kevin Mathew T <mathewtkevin@gmail.com>
2024-12-12 01:33:56 -0500

    i fucked up

--- models.py
+++ models.py
@@ -1460,15 +1460,15 @@ class Predictor2D(nn.Module):
         self.conv = nn.Sequential(
             nn.Conv2d(
                 self.config.out_c + 1,
-                self.config.out_c,
+                self.config.out_c * 4,
                 kernel_size=3,
                 stride=1,
                 padding=1,
             ),  # Combine state and action
             nn.ReLU(),
             nn.Conv2d(
-                self.config.out_c,
-                self.config.out_c,
+                self.config.out_c * 4,
+                self.config.out_c * 2,
                 kernel_size=3,
                 stride=1,
                 padding=1,
